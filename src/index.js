59eb7d0fe3c73e50b1c2bd3932a9251e39e47752
Anton Smolko <asmolko@yandex.ru>
2022-07-24 20:01:57 +0300

    implemented TF-IDF

--- src/index.js
+++ src/index.js
@@ -1,28 +1,63 @@
 import _ from 'lodash';
 
+const wordRegexp = /[\wа-яА-Я]+/gi;
+const getWordsCount = (doc) => {
+  const matches = doc.match(wordRegexp);
+  return _.get(matches, 'length', 0);
+};
+
 const searchEngine = (docs) => {
+  const dictionary = {};
+  const docsCount = docs.length;
+
+  const docsMap = docs.map((doc) => ({ ...doc, wordsCount: getWordsCount(doc.text) }));
+
   const getRegexp = (term) => new RegExp(`\\b${term}\\b`, 'gi');
 
+  const getDictionaryTerm = (term) => {
+    const termRegexp = getRegexp(term);
+
+    const { docsCountWithMatches, tfs } = docsMap.reduce((acc, { id, wordsCount, text }) => {
+      const matches = text.match(termRegexp);
+
+      if (_.isEmpty(matches)) { return acc; }
+
+      acc.docsCountWithMatches += 1;
+      acc.tfs = [...acc.tfs, { id, tf: matches.length / wordsCount }];
+
+      return acc;
+    }, { docsCountWithMatches: 0, tfs: [] });
+
+    const idf = Math.log(docsCount / docsCountWithMatches);
+
+    return tfs.reduce((acc, { id, tf }) => ({ ...acc, [id]: tf * idf }), {});
+  };
+
   return {
-    search: (query) => _.chain(docs)
-      .map(({ id, text }) => {
-        const tokens = query.split(' ');
-        const terms = tokens.flatMap((token) => token.match(/\w+/g));
-        const matches = _.chain(terms)
-          .map((term) => text.match(getRegexp(term)))
-          .compact()
-          .value();
+    search: (query) => {
+      const tokens = query.split(' ');
+      const terms = tokens.flatMap((token) => token.match(wordRegexp));
 
-        const matchesSetCount = matches.length
-        const totalMatchesCount = matches.flat().length
+      const tfIdfMap = terms.reduce((acc, term) => {
+        if (!dictionary[term]) {
+          dictionary[term] = getDictionaryTerm(term);
+        }
 
-        return { id, matchesSetCount, totalMatchesCount }
-      })
-      .filter('matchesSetCount')
-      .orderBy(['matchesSetCount', 'totalMatchesCount'], ['desc', 'desc'])
-      .map(({ id }) => id)
-      .value()
-  }
+        Object.entries(dictionary[term]).forEach(([id, tfIdf]) => {
+          acc[id] = acc[id] ? acc[id] + tfIdf : tfIdf;
+        });
+
+        return acc;
+      }, {});
+
+      return _.chain(tfIdfMap)
+        .entries()
+        .map(([id, tfIdf]) => ({ id, tfIdf }))
+        .orderBy('tfIdf', ['desc'])
+        .map('id')
+        .value();
+    },
+  };
 };
 
 export default searchEngine;
